e0093430059590d318c286c57e965b17b47a2685
Jerry Plotke <jerry.plotke@gmail.com>
2021-06-22 17:20:06 -0500

    commenting out for testing

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const fs = require('fs-extra');
-const inquirer = require('inquirer');
-const http = require('http');
+// const inquirer = require('inquirer');
+// const http = require('http');
 const app = express();
-const dbJSON = require('./db/db.json');
+// const dbJSON = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const path = require('path');
 
@@ -17,7 +17,7 @@ const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')))
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
