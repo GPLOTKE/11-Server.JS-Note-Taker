cc5a72c27fb4590cbeb8ce4c05cb9639da590779
Jerry Plotke <jerry.plotke@gmail.com>
2021-06-22 14:07:16 -0500

    trying to fix port heroku error

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const fs = require('fs-extra');
 const inquirer = require('inquirer');
 const http = require('http');
+var app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 const PORT = 3001;
 
@@ -11,6 +22,7 @@ const handleRequest = (request, response) => {
 
 const server = http.createServer(handleRequest);
 
+// server.listen(process.env.PORT || 3001)
 server.listen(PORT, () => {
     console.log(`Server listening on: http://localhost:${PORT}`);
 });
