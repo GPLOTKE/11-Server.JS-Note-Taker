84f53d1f1cb0503f7eafe4087168174a07d01743
Jerry Plotke <jerry.plotke@gmail.com>
2021-06-22 18:01:25 -0500

    changed file path

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/assets/index.html')))
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/assets/notes.html')))
 
 
 
