1d25abfff49799655fdd92745862f77a4362e7f1
Jerry Plotke <jerry.plotke@gmail.com>
2021-06-22 17:41:04 -0500

    removed .

--- server.js
+++ server.js
@@ -7,20 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path');
 
-// //For avoidong Heroku $PORT error
-// app.get('/', function(request, response) {
-//     var result = 'App is running'
-//     response.send(result);
-// }).listen(app.get('port'), function() {
-//     console.log('App is running, server is listening on port ', app.get('port'));
-// });
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')))
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
 
 
 
