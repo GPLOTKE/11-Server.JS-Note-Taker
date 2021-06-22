0737fd3c415a240ffe042963a56283736e7d7036
Jerry Plotke <jerry.plotke@gmail.com>
2021-06-22 13:42:42 -0500

    handlerequest function

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -5,6 +5,10 @@ const http = require('http');
 
 const PORT = 3001;
 
+const handleRequest = (request, response) => {
+    response.end(`It Works!! Path Hit: ${request.url}`);
+};
+
 const server = http.createServer(handleRequest);
 
 server.listen(PORT, () => {
