a878d0fee780de424ac40ef996fd03bd98fb74d6
Jerry Plotke <jerry.plotke@gmail.com>
2021-06-22 15:15:56 -0500

    body parsing

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fs = require('fs-extra');
+const inquirer = require('inquirer');
+const http = require('http');
+const app = express();
+const dbJSON = require('./Develop/db/db.json');
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+
+// //For avoidong Heroku $PORT error
+// app.get('/', function(request, response) {
+//     var result = 'App is running'
+//     response.send(result);
+// }).listen(app.get('port'), function() {
+//     console.log('App is running, server is listening on port ', app.get('port'));
+// });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')))
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});
+
+module.exports = server;
